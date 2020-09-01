f930a5df6138570684b2177dcaf4e223144eb77c
wuqingze <nongxiaolang@foxmail.com>
2020-09-01 17:15:37 +0800

    chapter 1

--- 1/forcount.cc
+++ 1/forcount.cc
@@ -34,7 +34,9 @@ int main()
     int sum = 0;
 
     // sum values from 1 through 10 inclusive
-    for (int val = 1; val <= 10; ++val) 
+    //for (int val = 1; val <= 10; ++val) 
+    //for (int val = 1; val <= 10; ++val) 
+    for (int val = 1; val <= 11; val++) 
         sum += val;  // equivalent to sum = sum + val
     std::cout << "Sum of 1 to 10 inclusive is " 
               << sum << std::endl;

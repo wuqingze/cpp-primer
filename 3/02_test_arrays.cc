bedec9c2aebbf75c6edd88ed37cf5c1686cde535
wuqingze <nongxiaolang@foxmail.com>
2020-09-10 21:58:04 +0800

    reference

---
+++ 3/02_test_arrays.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+// using declarations for names from the standard library
+using std::cin;
+using std::vector;
+using std::string;
+using std::toupper;
+using std::cout; using std::endl;
+
+int main(){
+
+    void test02_reference(int *i);
+    int i = 1;
+    printf("before foo i:%d\n", i);
+    test02_reference(&i);
+    printf("after foo i:%d\n", i);
+    return 0;
+
+    void test01_arrays_special();
+    test01_arrays_special();
+    return 0;
+
+    void test00_string_array();
+    test00_string_array();
+    return 0;
+}
+
+void test00_string_array(){
+    unsigned cnt = 24;
+    string bad[cnt];
+    printf("...end\n");
+}
+
+void test01_arrays_special(){
+    const char a4[6] = "Danie"; // error: no space for the null!
+}
+
+void test02_reference(int *i){
+    *i = 1024;
+}
